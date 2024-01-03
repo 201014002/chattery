28808a656aba333815fb5a1d63d864dd743fc864
Tayba <taybarahmankhan8@gmail.com>
2024-01-02 21:26:49 -0800

    Edited commit.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 //Widgets
 import 'package:chattery/widgets/custom_input_field.dart';
+import 'package:chattery/widgets/rounded_button.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -32,7 +33,12 @@ class _LoginPageState extends State<LoginPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             _pageTitle(),
+            SizedBox(height: _deviceHeight*0.04,),
             _loginForm(),
+            SizedBox(height: _deviceHeight*0.05,),
+            _loginButton(),
+            SizedBox(height: _deviceHeight*0.02,),
+            _registerAccountLink(),
           ],
         ),
       ),
@@ -72,6 +78,19 @@ class _LoginPageState extends State<LoginPage> {
                 hintText: "Password",
                 obscureText: true),
           ],
+        ),
+      ),
+    );
+  }
+  Widget _loginButton(){
+    return RoundedButton(name: "Login", height: _deviceHeight*0.065, width: _deviceWidth*0.65, onPressed: (){});
+  }
+  Widget _registerAccountLink(){
+    return GestureDetector(
+      onTap: (){},
+      child: Container(
+        child: Text(
+          'Don\'t have an account?',style: TextStyle(color: Colors.purple[200]),
         ),
       ),
     );
